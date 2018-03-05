e943a75c58707a3c34f105027a63245fe830df85
PAN Zhiqi <ericpoon510@gmail.com>
2018-03-05 12:24:31 +0800

    createUser function works

--- functions/create_user.js
+++ functions/create_user.js
@@ -1,4 +1,19 @@
+const admin = require('firebase-admin');
+
 module.exports = function(req, res) {
-  // req.body contains all the data passed to the function when the user calls it
-  res.send(req.body);
+  // Verify the user provided a phone
+  if (!req.body.phone) {
+    return res.status(422).send({error: 'Bad Input'});
+  }
+
+  // Format the phone number (might be number or string, so make it a string first) to remove dashes and parens
+  const phone = String(req.body.phone).replace(/[^\d]/g, "");
+
+  // Create a new user account using that phone number
+  admin.auth().createUser({ uid: phone })
+    .then(user => res.send(user)) // Respond to the user request, saying the account was created
+    .catch(err => res.status(422).send({ error: err }));
 }
+
+/* a weakness of using Google Clound Functions is the test cycle, 
+   we always have to deploy all changes before testing */
